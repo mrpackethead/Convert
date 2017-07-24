d42df7c5a32f814b09a30057b006cd39f548485a
Andrew Frazer <andrew.frazer@stellascapes.com>
2017-07-25 11:14:11 +1200

    created a configuration for the chockblock

--- basic/firmware/basic.X/nbproject/Makefile-variables.mk
+++ basic/firmware/basic.X/nbproject/Makefile-variables.mk
@@ -11,3 +11,10 @@ CND_ARTIFACT_PATH_udp_pic32mz_ef_sk=dist/udp_pic32mz_ef_sk/production/basic.X.pr
 CND_PACKAGE_DIR_udp_pic32mz_ef_sk=${CND_DISTDIR}/udp_pic32mz_ef_sk/package
 CND_PACKAGE_NAME_udp_pic32mz_ef_sk=basic.x.tar
 CND_PACKAGE_PATH_udp_pic32mz_ef_sk=${CND_DISTDIR}/udp_pic32mz_ef_sk/package/basic.x.tar
+# chocblock configuration
+CND_ARTIFACT_DIR_chocblock=dist/chocblock/production
+CND_ARTIFACT_NAME_chocblock=basic.X.production.hex
+CND_ARTIFACT_PATH_chocblock=dist/chocblock/production/basic.X.production.hex
+CND_PACKAGE_DIR_chocblock=${CND_DISTDIR}/chocblock/package
+CND_PACKAGE_NAME_chocblock=basic.x.tar
+CND_PACKAGE_PATH_chocblock=${CND_DISTDIR}/chocblock/package/basic.x.tar
